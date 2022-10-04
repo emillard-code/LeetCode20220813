3ffb2d977fadedca485dc68a22b5c0976784dedb
Emerson Millard <emillard.code@gmail.com>
2022-10-04 16:13:25 -0700

    Initial commit

---
+++ src/main/java/com/project/leetcode20220813/LeetCode20220813Application.java
@@ -0,0 +1,13 @@
+package com.project.leetcode20220813;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCode20220813Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetCode20220813Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/leetcode20220813/LeetCode20220813ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.leetcode20220813;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCode20220813ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
